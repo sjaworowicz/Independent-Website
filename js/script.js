e6e7a3e7f9459c2c1a45be248963b80acba4870f
sjaworowicz <125sjaworowicz@frhsd.com>
2025-03-17 09:47:01 -0400

    basically complete, except the javascript isnt working so trying to figure that out

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-function changeImage(fileName) {
+function changeImage (fileName){
     let img = document.querySelector("#bannerImage");
     img.setAttribute('src', fileName);
 }
 
 const paragraphChangeJS = document.getElementById("paragraphChange");
-function changeParagraph1 (paragraphChange) {
+function changeParagraph0 (paragraphChange){
     paragraphChangeJS.innerHTML = "Since the devils relocated from the Kansas City Scouts to the Colorado Rockies, to finally the devils, the won 3 Stanley cups in the 1994-95, 1999-00, and 2002-03 seasons.";
 }
 
-function changeParagraph2 (paragraphChange) {
-    paragraphChangeJS.innerHTML = "Throughout the devils history, only five numbers have been retired, which those numbers are Scott Stevenson’s #4, Ken Daneyko’s #3, Scott Niedrmayer’s #27, Martin Brodeur’s #30, and Patrik Elias’s #26.";
+function changeParagraph1 (paragraphChange){
+    paragraphChangeJS.innerHTML = "Throughout the devils history, only five numbers have been retired, which those numbers are Scott Stevensons #4, Ken Daneykos #3, Scott Niedrmayers #27, Martin Brodeurs #30, and Patrik Eliass #26.";
 }
 
-function changeParagraph3 (paragraphChange) {
+function changeParagraph2 (paragraphChange){
     paragraphChangeJS.innerHTML = "There are currently eleven players from the New Jersey Devils in the Hall of Fame, which are Martin Brodeur, Scott Stevens, Patrik Elias, Joe Nieuwendyk, Dave Andreychuk, Phil Housley, Brendan Shanahan, Igor Larionov, Doug Gilmour, Scott Niedermayer, and Viacheslav Fetisov. ";
 }
 
-function changeParagraph4 (paragraphChange) {
+function changeParagraph3 (paragraphChange){
     paragraphChangeJS.innerHTML = "Throughout the devils history, they became part of the Harris Blitzer Sports & Entertainment organization in 2013, had their best record in the 2022-23 season, and have made 5 conference championships and 9 division championships.";
 }
