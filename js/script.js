edc57314933353d190079d8e54a99481e6964aa1
sjaworowicz <125sjaworowicz@frhsd.com>
2025-03-20 09:45:34 -0400

    still trying to fix the javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-function changeImage (fileName){
-    let img = document.querySelector("#bannerImage");
+function changeImage(){
+    let img = document.getElementById("img");
+    console.log(img);
     img.setAttribute('src', fileName);
 }
 
@@ -8,14 +9,14 @@ function changeParagraph1 (paragraphChange){
     paragraphChangeJS.innerHTML = "Since the devils relocated from the Kansas City Scouts to the Colorado Rockies, to finally the devils, the won 3 Stanley cups in the 1994-95, 1999-00, and 2002-03 seasons.";
 }
 
-function changeParagraph2 (paragraphChange){
-    paragraphChangeJS.innerHTML = "Throughout the devils history, only five numbers have been retired, which those numbers are Scott Stevensons #4, Ken Daneykos #3, Scott Niedrmayers #27, Martin Brodeurs #30, and Patrik Eliass #26.";
-}
+// function changeParagraph2 (paragraphChange){
+//     paragraphChangeJS.innerHTML = "Throughout the devils history, only five numbers have been retired, which those numbers are Scott Stevensons #4, Ken Daneykos #3, Scott Niedrmayers #27, Martin Brodeurs #30, and Patrik Eliass #26.";
+// }
 
-function changeParagraph3 (paragraphChange){
-    paragraphChangeJS.innerHTML = "There are currently eleven players from the New Jersey Devils in the Hall of Fame, which are Martin Brodeur, Scott Stevens, Patrik Elias, Joe Nieuwendyk, Dave Andreychuk, Phil Housley, Brendan Shanahan, Igor Larionov, Doug Gilmour, Scott Niedermayer, and Viacheslav Fetisov. ";
-}
+// function changeParagraph3 (paragraphChange){
+//     paragraphChangeJS.innerHTML = "There are currently eleven players from the New Jersey Devils in the Hall of Fame, which are Martin Brodeur, Scott Stevens, Patrik Elias, Joe Nieuwendyk, Dave Andreychuk, Phil Housley, Brendan Shanahan, Igor Larionov, Doug Gilmour, Scott Niedermayer, and Viacheslav Fetisov. ";
+// }
 
-function changeParagraph4 (paragraphChange){
-    paragraphChangeJS.innerHTML = "Throughout the devils history, they became part of the Harris Blitzer Sports & Entertainment organization in 2013, had their best record in the 2022-23 season, and have made 5 conference championships and 9 division championships.";
-}
+// function changeParagraph4 (paragraphChange){
+//     paragraphChangeJS.innerHTML = "Throughout the devils history, they became part of the Harris Blitzer Sports & Entertainment organization in 2013, had their best record in the 2022-23 season, and have made 5 conference championships and 9 division championships.";
+// }
